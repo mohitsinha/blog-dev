4f922c4ab498b671c1ac52a6222d8430a905b92d
Mohit Sinha <mohitsinha.it@gmail.com>
2017-09-08 01:21:08 +0530

    Blog for 2017

--- js/site.js
+++ js/site.js
@@ -4,7 +4,7 @@ $(function(){
   // NOTE: this code is mad ghetto and in no wise to be held up as a standard of JavaScript brilliance; so sorry bout that; but It Works™
 
   // first, hide everything except 2015 posts be default
-  $(".article-item:not(.article-2016)").removeClass("show");
+  $(".article-item:not(.article-2017)").removeClass("show");
 
   // if visitor clicks on All, show all posts
   $("#articlesAllTrigger").on("click", function(event){
@@ -15,12 +15,21 @@ $(function(){
   });
 
   // if visitor clicks on 2015, show 2015 posts
-  $("#articles2016Trigger").on("click", function(event){
+  $("#articles2017Trigger").on("click", function(event){
     event.preventDefault();
     $(".article-nav .year").removeClass("showing");
     $(this).addClass("showing");
     $(".article-item").removeClass("show");
-    $(".article-item.article-2016").addClass("show");
+    $(".article-item.article-2017").addClass("show");
+  });
+
+  // if visitor clicks on 2015, show 2015 posts
+  $("#articles2016Trigger").on("click", function(event){
+      event.preventDefault();
+      $(".article-nav .year").removeClass("showing");
+      $(this).addClass("showing");
+      $(".article-item").removeClass("show");
+      $(".article-item.article-2016").addClass("show");
   });
 
   // if visitor clicks on 2015, show 2015 posts
